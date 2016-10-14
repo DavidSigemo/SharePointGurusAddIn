7da2ca7db3767e57eb9e5b26af6c64f7405aa13a
rozhna ramak <rojyjoon@yahoo.com>
2016-10-14 13:07:07 +0200

    .

--- SharePointWeatherAddIn/SharePointWeatherAddIn/Scripts/WebPartScript.js
+++ SharePointWeatherAddIn/SharePointWeatherAddIn/Scripts/WebPartScript.js
@@ -1,10 +1,8 @@
 ﻿$(document).ready(function () {
-    var url = "https://api.darksky.net/forecast/6ebbfb6cba7bb3d4c1b0d03800b23abe/59.3446,18.0237"
-    $.ajax({
-        url: url,
-        dataType: "jsonp",
-        success: function (responseData) {
-            initGraph(responseData);
-        }
+    $.get("https://api.darksky.net/forecast/6ebbfb6cba7bb3d4c1b0d03800b23abe/59.3446,18.0237", function (response) {
+        console.log(response);
+        $("#name").text(response.name);
+        $("#temp").text(response.main.temp);
+        $("#humidity").text(response.main.humidity);
     });
 });
